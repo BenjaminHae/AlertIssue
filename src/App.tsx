a4588d34d66d9cf09419d1d0065f5f67050da74e
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-07-28 18:13:22 +0200

    introduces error

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 
 function App() {
+  const [show, setShow] = React.useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<Container>
+		<Button onClick={()=>setShow(!show)}>Toggle Alert</Button>
+		<Alert show={show} variant="info" transition={Collapse} >Foo</Alert>
+	</Container>
   );
 }
 
